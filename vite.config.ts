1b87c851ca60efdbba5da68314542e03e415b475
huseyinozcanasta <20220100134@ogr.akdeniz.edu.tr>
2026-04-08 16:52:51 +0300

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vitest/config";
 
 // https://vitejs.dev/config/
 export default defineConfig(() => ({
-  base: "/Deneme-ders/",
+  base: './',
   
   server: {
     host: "::",
