61a133a980e161e40232cc8ce4cce185b7713220
huseyinozcanasta <20220100134@ogr.akdeniz.edu.tr>
2026-04-08 16:40:30 +0300

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import { defineConfig } from "vitest/config";
 
 // https://vitejs.dev/config/
 export default defineConfig(() => ({
+  base: "/Deneme-ders/",
+  
   server: {
     host: "::",
     port: 8080,
